3d806dbb14e4169e801035322c0fd2bca991d2ab
unknown <BT_4N2_23@BKTECH.LOCAL>
2022-09-16 12:06:18 -0400

    Initial commit

---
+++ src/Digits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Digits {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a five digit number: ");
+        int num = s.nextInt();
+        int tenThousandDigit = (num / 10000 + 1) % 10;
+        int thousandDigit = (num % 10000 / 1000 + 1) % 10;
+        int hundredDigit = (num % 1000 / 100 + 1) % 10;
+        int tenDigit = (num % 100 / 10 + 1) % 10;
+        int oneDigit = (num % 10 + 1) % 10;
+        System.out.println("Number: " + tenThousandDigit + thousandDigit + hundredDigit + tenDigit + oneDigit);
+
+    }
+}
